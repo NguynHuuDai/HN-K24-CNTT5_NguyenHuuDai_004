f6cf9f5d7775f89640c53ad9c20e46ac8dee88e9
NguynHuuDai <daizzt10@gmail.com>
2026-03-05 09:45:33 +0700

    Add files via upload

---
+++ HN-K24-CNTT5_NguyenHuuDai_004/src/Bai1/Main.java
@@ -0,0 +1,35 @@
+package Bai1;
+
+import java.util.*;
+//lưu danh sách bằng hàm list từ 0.0 - 10.0
+//bouble sort để lọc ra điểm vao nhất và điểm nhất lớp
+// set lọc mức điểm số khác nhau
+// dùng tìm điểm cao nhất và thấp nhất
+// tính tổng điểm cả lớp và điểm trung bình của các sinh viên
+// danh sách điểm tăng dần (không trùng)
+//đếm sinh viên đạt laoij giỏi (điểm > 8);
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào số lượng sinh viên");
+        int input = sc.nextInt();
+        if (input <= 0){
+            System.err.println("Số lượng sinh viên không hợp lệ");
+
+        }
+
+
+
+           }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ HN-K24-CNTT5_NguyenHuuDai_004/src/Bai2/Main.java
@@ -0,0 +1,19 @@
+package Bai2;
+import java.util.*;
+// nhập vào một chuỗi lịch sử cách nhau dấu phẩy
+// thống kê số lần xuất hiện không phân biệt hoa thường bỏ khoảng trắng thừa
+// người dùng nhập tên loại phương tiện để tra cứu lượng lượt ra vào
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Map<String, Integer> map = new HashMap<>();
+
+        String history = sc.nextLine();
+
+
+
+
+    }
+
+
+}
